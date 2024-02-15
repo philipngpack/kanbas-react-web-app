ae05232adb1e2996149fd197d96520d28dc32792
philipngpack <ngpack.p@northeastern.edu>
2024-02-15 18:22:21 -0500

    up to arrays

--- src/Labs/a3/Javascript/functions/WorkingWithFunctions/index.tsx
+++ src/Labs/a3/Javascript/functions/WorkingWithFunctions/index.tsx
@@ -1,11 +1,15 @@
 import ES5Functions from "../ES5Functions";
 import ArrowFunctions from "../ArrowFunctions";
+import ImpliedReturns from "../ImpliedReturn";
+import FunctionParenthesisAndParameters from "../FunctionParenthesisAndParameters";
 
 function WorkingWithFunctions() {
     return (
         <div>
             <ES5Functions />
             <ArrowFunctions />
+            <ImpliedReturns />
+            <FunctionParenthesisAndParameters />
         </div>
     );
 }
