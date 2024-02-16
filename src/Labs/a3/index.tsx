33245f9700cf6b71947a99db08ae9b7ede097b5f
philipngpack <ngpack.p@northeastern.edu>
2024-02-15 20:45:47 -0500

    styles as a function

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,11 +1,13 @@
 import JavaScript from "./Javascript";
 import PathParameters from "./routing/PathParameters";
 import Classes from "./Classes";
+import Styles from "./Styles";
 
 function Assignment3() {
     return (
         <div>
             <h1>Assignment 3</h1>
+            <Styles />
             <Classes />
             <PathParameters />
             <JavaScript />
