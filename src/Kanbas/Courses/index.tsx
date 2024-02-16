2bcfb169ae0628e4186e848f75bdb60fcaffd791
philipngpack <ngpack.p@northeastern.edu>
2024-02-16 16:42:58 -0500

    add all data

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -2,6 +2,7 @@ import { courses } from "../../Kanbas/Database";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
+import Modules from "./Modules";
 import "./index.css";
 
 function Courses() {
@@ -19,7 +20,7 @@ function Courses() {
                     <Routes>
                         <Route path="/" element={<Navigate to="Home" />} />
                         <Route path="Home" element={<h1>Home</h1>} />
-                        <Route path="Modules" element={<h1>Modules</h1>} />
+                        <Route path="Modules" element={<Modules/>} />
                         <Route path="Piazza" element={<h1>Piazza</h1>} />
                         <Route path="Assignments" element={<h1>Assignments</h1>} />
                         <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
