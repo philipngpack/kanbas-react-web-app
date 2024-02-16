eb02f1af0f491a238b6f26c7f9edf16e4044b908
philipngpack <ngpack.p@northeastern.edu>
2024-02-15 20:49:33 -0500

    conditionals

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -2,11 +2,13 @@ import JavaScript from "./Javascript";
 import PathParameters from "./routing/PathParameters";
 import Classes from "./Classes";
 import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
 
 function Assignment3() {
     return (
         <div>
             <h1>Assignment 3</h1>
+            <ConditionalOutput/>
             <Styles />
             <Classes />
             <PathParameters />
