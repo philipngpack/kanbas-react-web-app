909e8de9eb83583d30173c32ca6616a14e28c48a
philipngpack <ngpack.p@northeastern.edu>
2024-02-23 20:51:44 -0500

    general styling

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -10,7 +10,7 @@ function Kanbas() {
             <div style={{ flexGrow: 1 }}>
                 <Routes>
                     <Route path="/" element={<Navigate to="Dashboard" />} />
-                    <Route path="Account" element={<h1>Account</h1>} />
+                    <Route path="Account" element={<h1>&nbsp; Account</h1>} />
                     <Route path="Dashboard" element={<Dashboard />} />
                     <Route path="Courses/:courseId/*" element={<Courses />} />
                 </Routes>
