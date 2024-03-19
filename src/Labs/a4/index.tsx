15537952be1505a564a69c7ef520bf734c9768cc
philipngpack <ngpack.p@northeastern.edu>
2024-03-19 11:34:11 -0400

    real 2.3.3

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+
+const Assignment4 = () => {
+    function sayHello() {
+        alert("Hello");
+    }
+    return (
+        <>
+            <h1>Assignment 4</h1>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+
+        </>
+    );
+};
+
+export default Assignment4;
